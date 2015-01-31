76fbe25a3040300a1105c38236b36dd0eaf36e52
DavideDaniel <davidedaniel@gmail.com>
2015-01-31 16:31:47 -0500

    music manager

---
+++ seed.rb
@@ -0,0 +1,55 @@
+require 'pry'
+require_relative './lib/connection'
+require_relative './lib/song'
+require_relative './lib/album'
+require_relative './lib/artist'
+
+
+silversun = Artist.create({
+  name: "Silversun Pickups",
+})
+
+neck_of_the_woods = Album.create({
+  title: "Neck of the Woods",
+  artist_id: silversun.id
+})
+
+Song.create({
+  title: "Skin Graph",
+  album_id: neck_of_the_woods.id
+})
+
+Song.create({
+  title: "The Pit",
+  album_id: neck_of_the_woods.id
+})
+
+Song.create({
+  title: "Simmer",
+  album_id: neck_of_the_woods.id
+})
+
+bastille = Artist.create({
+  name: "Bastille",
+})
+
+bad_blood = Album.create({
+  title: "Bad Blood",
+  artist_id: silversun.id
+})
+
+Song.create({
+  title: "Pompeii",
+  album_id: bad_blood.id
+})
+
+Song.create({
+  title: "Things we lost in the fire",
+  album_id: bad_blood.id
+})
+
+Song.create({
+  title: "Bad Blood",
+  album_id: bad_blood.id
+})
+
---
+++ server.rb
@@ -0,0 +1,122 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require_relative './lib/connection'
+require_relative './lib/artist'
+require_relative './lib/album'
+require_relative './lib/song'
+require 'pry'
+
+
+after do
+  ActiveRecord::Base.connection.close
+end
+
+get("/") do
+  erb(:index)
+end
+get("/songs") do
+  erb(:"songs/index", { locals: { songs: Song.all(), } })
+end
+get("/songs/new") do
+  erb(:"songs/new", { locals: { albums: Album.all(), } })
+end
+
+post("/songs") do
+  album_hash = {
+    title: params["title"],
+    album_id: params["album_id"]
+  }
+
+  Song.create(song_hash)
+
+  erb(:"songs/index", { locals: { albums: Album.all() } })
+end
+
+get("/songs/:id") do
+  song = Song.find_by({id: params[:id]})
+  erb(:"songs/show", { locals: { song: song } })
+end
+
+get("/songs/:id/edit") do
+  song = Song.find_by({id: params[:id]})
+  erb(:"songs/edit", { locals: { song: song , albums: Album.all()} })
+end
+
+put("/songs/:id") do
+  song_hash = {
+    title: params["title"],
+    album_id: params["album_id"]
+  }
+
+  song = Album.find_by({id: params[:id]})
+  song.update(song_hash)
+
+  erb(:"songs/show", { locals: { song: song } })
+end
+delete("/songs/:id") do
+  song = Song.find_by({id: params[:id]})
+  song.destroy
+
+  redirect "/songs"
+end
+
+
+
+get("/albums") do
+  erb(:"albums/index", { locals: { albums: Album.all(), } })
+end
+
+
+
+post("/albums") do
+  album_hash = {
+    title: params["title"],
+    artist_id: params["artist_id"]
+  }
+
+  Album.create(album_hash)
+
+  erb(:"albums/index", { locals: { albums: Album.all() } })
+end
+
+
+get("/albums/:id") do
+  album = Album.find_by({id: params[:id]})
+  erb(:"albums/show", { locals: { album: album } })
+end
+
+
+get("/albums/:id/edit") do
+  album = Album.find_by({id: params[:id]})
+  erb(:"albums/edit", { locals: { album: album, artists: Artist.all() } })
+end
+
+put("/albums/:id") do
+  album_hash = {
+    title: params["title"],
+    artist_id: params["artist_id"]
+  }
+
+  album = Album.find_by({id: params[:id]})
+  album.update(album_hash)
+
+  erb(:"albums/show", { locals: { album: album } })
+end
+
+delete("/albums/:id") do
+  album = Album.find_by({id: params[:id]})
+  album.destroy
+
+  redirect "/albums"
+end
+
+
+get("/artists") do
+
+  erb(:"artists/index", { locals: { artists: Artist.all() } })
+end
+
+get("/artists/:id") do
+  artist = Artist.find_by({id: params[:id]})
+  erb(:"artists/show", { locals: { artist: artist } })
+end
